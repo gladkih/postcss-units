87fa9e4329ff1c1254546145f499b05cf77c4188
Maxim Gladkih <mail@gladkih.su>
2016-02-25 13:37:54 +0300

    codestyle

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ var regularType = /\brem|\bem/;
 var postcss = require('postcss');
 
 function postcssUnits(options) {
-	options = extend({
-		size: 16,
-		fallback: false,
-		precision: 3
-	}, options);
+  options = extend({
+    size: 16,
+    fallback: false,
+    precision: 3
+  }, options);
 
-	if (options.size === 0) {
-		options.size = 1;
-	}
+  if (options.size === 0) {
+    options.size = 1;
+  }
 
-	return function(css) {
+  return function(css) {
 
-		css.walkDecls(function(decl, i) {
-			var value = decl.value;
-			var parsing = value.match(regular);
-			var type;
+    css.walkDecls(function(decl, i) {
+      var value = decl.value;
+      var parsing = value.match(regular);
+      var type;
 
-			if (parsing) {
-				options.elementSize = parsing[0].match(regularNumber)[0];
-				type = parsing[0].match(regularType)[0];
+      if (parsing) {
+        options.elementSize = parsing[0].match(regularNumber)[0];
+        type = parsing[0].match(regularType)[0];
 
-				switch (type) {
-					case 'rem':
-						typeRem(decl, options, i);
-						break;
-					case 'em':
-						typeEm(decl, options);
-						break;
-					default:
-						break;
-				}
-			}
-		});
-	};
+        switch (type) {
+          case 'rem':
+            typeRem(decl, options, i);
+            break;
+          case 'em':
+            typeEm(decl, options);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  };
 }
 
 /**
@@ -49,8 +49,8 @@ function postcssUnits(options) {
  * @param options
  */
 function typeEm(decl, options) {
-	var size = numberAfterPoint(options.elementSize / options.size, options.precision);
-	decl.value = decl.value.replace(regular, size + 'em');
+  var size = numberAfterPoint(options.elementSize / options.size, options.precision);
+  decl.value = decl.value.replace(regular, size + 'em');
 }
 
 /**
@@ -60,28 +60,28 @@ function typeEm(decl, options) {
  * @param ruleNumber — номер правила по порядку
  */
 function typeRem(decl, options, ruleNumber) {
-	var size = numberAfterPoint(options.elementSize / options.size, options.precision);
-	var value = decl.value.replace(regular, size + 'rem');
+  var size = numberAfterPoint(options.elementSize / options.size, options.precision);
+  var value = decl.value.replace(regular, size + 'rem');
 
-	if (options.fallback) {
-		decl.value = decl.value.replace(regular, options.elementSize + 'px');
-		decl.parent.insertAfter(ruleNumber, decl.clone({value: value}));
-	} else {
-		decl.value = value;
-	}
+  if (options.fallback) {
+    decl.value = decl.value.replace(regular, options.elementSize + 'px');
+    decl.parent.insertAfter(ruleNumber, decl.clone({value: value}));
+  } else {
+    decl.value = value;
+  }
 
 }
 
 /**
- * Функция для округления чисел после запятой
- * @param number исходное число
- * @param precision сколько знаков после запятой должно быть
- * @returns {number} итоговое число
+ * The function for rounding of numbers after the decimal point
+ * param Number the original number
+ * param Precision how many decimal places should be
+ * returns {Number} final number
  */
 function numberAfterPoint(number, precision) {
-	var multiplier = Math.pow(10, precision + 1);
-	var fullNumber = Math.floor(number * multiplier);
-	return Math.round(fullNumber / 10) * 10 / multiplier;
+  var multiplier = Math.pow(10, precision + 1);
+  var fullNumber = Math.floor(number * multiplier);
+  return Math.round(fullNumber / 10) * 10 / multiplier;
 }
 
 module.exports = postcss.plugin('postcss-units', postcssUnits);
