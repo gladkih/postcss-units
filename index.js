e517e8d574f964dfa584a3c6840bb6d40ab54f59
Bogdan Chadkin <trysound@yandex.ru>
2016-09-03 22:17:04 +0300

    Prefer value parsing. Add multiple functions in one value support

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var extend = require('extend');
-var regular = /(\brem\(|\bem\()(:?\-|)(\d*\.?\d+)?(px|)(\))/ig;
-var regularNumber = /(:?\-|)\d*\.?\d+/;
-var regularType = /\brem|\bem/;
 var postcss = require('postcss');
+var valueParser = require('postcss-value-parser');
 
 function postcssUnits(options) {
   options = extend({
@@ -18,57 +16,61 @@ function postcssUnits(options) {
   }
 
   return function(css) {
-
     css.walkDecls(function(decl) {
-      var value = decl.value;
-      var parsing = value.match(regular);
-      var type;
+      var fallback = false;
 
-      if (parsing) {
-        var number = parsing[0].match(regularNumber)[0];
-        type = parsing[0].match(regularType)[0];
-
-        switch (type) {
-          case 'rem':
-            typeRem(decl, number, options);
-            break;
-          case 'em':
-            typeEm(decl, number, options);
-            break;
+      var parsedValue = valueParser(decl.value).walk(function(node) {
+        if (!isValidFunction(node)) {
+          return;
         }
+
+        var value = valueParser.unit(node.nodes[0].value);
+        if (!isValidUnit(value)) {
+          return;
+        }
+
+        var type = node.value;
+        var number = Number(value.number);
+        var size = convert(number, options);
+        node.type = 'word';
+        node.value = size + type;
+
+        if (options.fallback && type === 'rem') {
+          node.fallback = value.number + value.unit;
+          fallback = true;
+        }
+      });
+
+      decl.value = parsedValue.toString();
+
+      if (fallback) {
+        decl.cloneBefore({
+          value: parsedValue.walk(function(node) {
+            if (node.fallback) {
+              node.value = node.fallback;
+            }
+          }).toString()
+        });
       }
     });
   };
 }
 
-/**
- * Change from em(«number») to «number»em
- * @param decl
- * @param options - replace options
- */
-function typeEm(decl, number, options) {
-  var size = getSize(number, options);
-  decl.value = decl.value.replace(regular, size + 'em');
+function isValidFunction(node) {
+  var functions = {
+    em: true,
+    rem: true
+  };
+  return node.type === 'function' &&
+         functions[node.value] &&
+         node.nodes[0].type === 'word';
 }
 
-/**
- * Change from rem(«number») to «number»rem
- * @param decl
- * @param options - replace options
- */
-function typeRem(decl, number, options) {
-  var size = getSize(number, options);
-
-  if (options.fallback) {
-    decl.cloneBefore({
-      value: decl.value.replace(regular, number + 'px')
-    });
-  }
-
-  decl.value = decl.value.replace(regular, size + 'rem');
+function isValidUnit(value) {
+  return !value.unit || value.unit === 'px';
 }
 
-function getSize(number, options) {
+function convert(number, options) {
   return numberAfterPoint(number / options.size, options.precision);
 }
 
